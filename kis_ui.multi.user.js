a5eaa4c9b69eeeba76c6e4397752bbe37e4ada2f
mk-pmb <mk-pmb@users.noreply.github.com>
2014-11-05 19:51:31 +0100

    add in-file license notes

--- kis_ui.multi.user.js
+++ kis_ui.multi.user.js
@@ -11,8 +11,27 @@
 // @resource    bkml:00.002 kis_ui.domain_gropups.js
 // ==/UserScript==
 
+/***** LICENSE: GNU GPL v2 *****\
+KIS UI utils: Make Host Europe's KIS more comfortable
+Copyright (C) 2014  M. Krause <http://pimpmybyte.de/>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+\***** ***** ***** ***** ***** *****/
+
 'use strict';
-var GM = this, noop = function () { return; };
+var GM = this;
 
 GM.enqueueBookmarkletOutsideSandbox = function (jsfunc) {
   /* Push a bookmarklet to avoid having to use unsafeWindow.
